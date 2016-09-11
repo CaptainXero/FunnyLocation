8e7dab3e8b4a497864180a0a4d104ae8658ba2e1
zhaoyi <297756521@qq.com>
2016-09-11 10:04:23 +0800

    HomeActivity Finish

--- app/src/main/java/com/example/administrator/ui/HomeActivity.java
+++ app/src/main/java/com/example/administrator/ui/HomeActivity.java
@@ -1,18 +1,155 @@
 package com.example.administrator.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.administrator.entity.AppInfo;
+import com.example.administrator.hook.BuildConfig;
 import com.example.administrator.hook.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
+    private ProgressDialog mProgressDialog;
+    private AppAdapter mAppAdapter;
+    private ArrayList<AppInfo> mAppInfos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Intent intent = new Intent(HomeActivity.this,MainActivity.class);
-        startActivity(intent);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_app);
+        assert recyclerView != null;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setHasFixedSize(true);
+        mAppAdapter = new AppAdapter(mAppInfos);
+        recyclerView.setAdapter(mAppAdapter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setMax(100);
+        mProgressDialog.setMessage("正在扫描应用程序");
+        mProgressDialog.show();
+
+        GetAppInfoTask getAppInfoTask = new GetAppInfoTask();
+        getAppInfoTask.execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.setting:
+
+                break;
+            case R.id.donate:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.jianshu.com/p/fce51137a835")));
+                break;
+            case R.id.about:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.jianshu.com/users/d0cb315fe1ae/latest_articles")));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    class AppAdapter extends RecyclerView.Adapter<AppAdapter.AppViewHolder>{
+
+        ArrayList<AppInfo> mAppInfos;
+
+        AppAdapter(ArrayList<AppInfo> appInfos) {
+            this.mAppInfos = appInfos;
+        }
+
+        @Override
+        public AppAdapter.AppViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new AppViewHolder(getLayoutInflater().inflate(R.layout.app_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(final AppAdapter.AppViewHolder holder, int position) {
+            holder.ivIcon.setImageDrawable(mAppInfos.get(position).getAppIcon());
+            holder.tvName.setText(mAppInfos.get(position).getAppName());
+            holder.tvPackageName.setText(mAppInfos.get(position).getPackageName());
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(HomeActivity.this, MainActivity.class).putExtra("package_name", mAppInfos.get(holder.getAdapterPosition()).getPackageName()));
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mAppInfos.size();
+        }
+        class AppViewHolder extends RecyclerView.ViewHolder {
+
+            ImageView ivIcon;
+            TextView tvName;
+            TextView tvPackageName;
+
+            AppViewHolder(View itemView) {
+                super(itemView);
+                ivIcon = (ImageView) itemView.findViewById(R.id.iv_icon);
+                tvName = (TextView) itemView.findViewById(R.id.tv_name);
+                tvPackageName = (TextView) itemView.findViewById(R.id.tv_package_name);
+            }
+        }
+    }
+    private class GetAppInfoTask extends AsyncTask<Integer, Integer, ArrayList<AppInfo>> {
+
+        @Override
+        protected ArrayList<AppInfo> doInBackground(Integer[] params) {
+            ArrayList<AppInfo> appList = new ArrayList<>();
+            List<PackageInfo> packages = getPackageManager().getInstalledPackages(0);
+            for (int i = 0; i < packages.size(); i++) {
+                PackageInfo packageInfo = packages.get(i);
+                AppInfo tmpInfo = new AppInfo();
+                tmpInfo.appName = packageInfo.applicationInfo.loadLabel(getPackageManager()).toString();
+                tmpInfo.packageName = packageInfo.packageName;
+                tmpInfo.versionName = packageInfo.versionName;
+                tmpInfo.versionCode = packageInfo.versionCode;
+                tmpInfo.appIcon = packageInfo.applicationInfo.loadIcon(getPackageManager());
+                if (!packageInfo.packageName.equals(BuildConfig.APPLICATION_ID))
+                    appList.add(tmpInfo);
+                publishProgress(i / packages.size() * 100);
+            }
+            return appList;
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            mProgressDialog.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<AppInfo> o) {
+            mProgressDialog.dismiss();
+            mAppInfos.addAll(o);
+            mAppAdapter.notifyDataSetChanged();
+        }
     }
 }
