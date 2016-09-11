560aa179e84d25f26379d46aee957e18a11a34db
zhaoyi <297756521@qq.com>
2016-09-11 15:34:07 +0800

    V1.0 finished(but without locationMark)

--- app/src/main/java/com/example/administrator/hook/FakeGPS.java
+++ app/src/main/java/com/example/administrator/hook/FakeGPS.java
@@ -2,55 +2,45 @@ package com.example.administrator.hook;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 
-import com.example.administrator.ui.MainActivity;
 import com.example.administrator.utils.DbHelper;
+
+import java.util.Random;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-import static com.example.administrator.ui.MainActivity.mylatitude;
-import static com.example.administrator.ui.MainActivity.mylongitude;
 
 /**
  * Created by Administrator on 2016/9/1 0001.
  */
 
 public class FakeGPS implements IXposedHookLoadPackage{
-    private int lac = 0;
-    private int cid = 0;
+
     private DbHelper myDbHelper;
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        final Object activityThread = XposedHelpers.callStaticMethod(XposedHelpers.findClass("android.app.ActivityThread", null), "currentActivityThread");
+        final Object activityThread = XposedHelpers.callStaticMethod(XposedHelpers
+                .findClass("android.app.ActivityThread", null), "currentActivityThread");
         final Context systemContext = (Context) XposedHelpers.callMethod(activityThread, "getSystemContext");
-        //过滤掉包名不是 com.tencent.mm(微信) 的应用
-        String packname = "com.tencent.mm";
-        String correntPackage = loadPackageParam.packageName.toString();
-        if(!correntPackage.equals(packname)) {
-            return;
-        }else {
-            XposedBridge.log("Loaded_APP: " + loadPackageParam.packageName);
+        Uri uri = Uri.parse("content://com.example.administrator.utils.AppInfoProvider/app");
+        Cursor cursor = systemContext.getContentResolver().query(uri, new String[]{"latitude", "longitude","lac","cid"}
+                , "package_name=?", new String[]{loadPackageParam.packageName}, null);
+        if (cursor != null && cursor.moveToNext()) {
+            //41019, 18511
+            double latitude = cursor.getDouble(cursor.getColumnIndex("latitude"))
+                    + (double) new Random().nextInt(100) / 1000000 + ((double) new Random().nextInt(99999999)) / 100000000000000d;
+            double longitude = cursor.getDouble(cursor.getColumnIndex("longitude"))
+                    + (double) new Random().nextInt(100) / 1000000 + ((double) new Random().nextInt(99999999)) / 100000000000000d;
+            int lac = cursor.getInt(cursor.getColumnIndex("lac"));
+            int cid = cursor.getInt(cursor.getColumnIndex("cid"));
+            XposedBridge.log("模拟位置:" + loadPackageParam.packageName + "," + latitude + "," + longitude + "," + lac + "," + cid);
+            HookUtils.HookAndChange(loadPackageParam.classLoader, latitude, longitude, lac, cid);
+            cursor.close();
         }
-        SQLiteDatabase db = new DbHelper(systemContext).getWritableDatabase();
-        Cursor cursor = db.query("Location",null,null,null,null,null,null);
-        if(cursor.moveToFirst()){
-            do {
-                Double la = cursor.getDouble(cursor.getColumnIndex("latitude"));
-                Double lon = cursor.getDouble(cursor.getColumnIndex("longitude"));
-                XposedBridge.log("DB_Location: " + "latitude" + la + "\n" + "longitude" + lon);
-            }while (cursor.moveToNext());
-        }
-        cursor.close();
-
-        HookUtils.HookAndChange(loadPackageParam.classLoader,mylatitude,mylongitude, lac, cid);
-
-        XposedBridge.log("Hook_Finish: " + loadPackageParam.packageName + "," + mylatitude + "," + mylongitude + "," + lac + "," + cid);
-
     }
-
 
 }
--- app/src/main/java/com/example/administrator/ui/MainActivity.java
+++ app/src/main/java/com/example/administrator/ui/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.administrator.ui;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.baidu.mapapi.SDKInitializer;
@@ -12,29 +21,50 @@ import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapPoi;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.model.LatLng;
 
 import com.example.administrator.hook.R;
 import com.example.administrator.utils.DbHelper;
 
+import java.lang.reflect.Field;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements BaiduMap.OnMapClickListener{
     private MapView mapView;
     private BaiduMap mBaidumap;
     private BitmapDescriptor bitmapDescriptor;
-    private String address = "";
-    public static double mylatitude = 39.9531;
-    public static double mylongitude = 116.2243;
-    private SQLiteDatabase mySQLiteDb;
+    private int lac = 0, cid = 0;
+    private String pacakgeName;
+    private SQLiteDatabase mSQLiteDatabase;
+    private LatLng latLng;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        pacakgeName = getIntent().getStringExtra("package_name");
+        mSQLiteDatabase = new DbHelper(this).getWritableDatabase();
+        Cursor cursor = mSQLiteDatabase.query(DbHelper.APP_TABLE_NAME, new String[]{"latitude,longitude"}, "package_name=?", new String[]{pacakgeName}, null, null, null);
+        if (cursor != null && cursor.moveToNext()) {
+            //拿到经纬度
+            double lat = cursor.getDouble(cursor.getColumnIndex("latitude"));
+            double lon = cursor.getDouble(cursor.getColumnIndex("longitude"));
+            //设置标记
+            LatLng latLng1 = new LatLng(lat, lon);
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng1);
+            markerOptions.draggable(true);
+            markerOptions.title("经度：" + latLng1.longitude + ",纬度：" + latLng1.latitude);
+            cursor.close();
+
+        }
+        pacakgeName = getIntent().getStringExtra("package_name");
         //地图控件引用
         mapView = (MapView) findViewById(R.id.map_view);
         mBaidumap = mapView.getMap();
@@ -47,39 +77,109 @@ public class MainActivity extends AppCompatActivity {
 
         //设置标记图标
         bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.id.map_view);
+        mBaidumap.setOnMapClickListener(this);
+    }
 
-        mBaidumap.setOnMapClickListener(new BaiduMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                mySQLiteDb = new DbHelper(MainActivity.this).getWritableDatabase();
-                ContentValues values = new ContentValues();
-                //点击地图事件监听
-                //获取经纬度
-                double latitude = latLng.latitude;
-                double longitude = latLng.longitude;
-                values.put("latitude",latitude);
-                values.put("longitude",longitude);
-                mySQLiteDb.insert("Location",null,values);
-                values.clear();
+    @Override
+    public void onMapClick(LatLng latLng) {
+        mBaidumap.clear();
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.draggable(true);
+        markerOptions.title("经度：" + latLng.longitude + ",纬度：" + latLng.latitude);
+//        mBaidumap.addMarker(markerOptions);
+        this.latLng = latLng;
+    }
 
-                mylatitude = latitude;
-                mylongitude = longitude;
+    @Override
+    public boolean onMapPoiClick(MapPoi mapPoi) {
+        return false;
+    }
 
-                Toast.makeText(MainActivity.this,"latitude: "+mylatitude+"\n"+"longitude: "+mylongitude,Toast.LENGTH_SHORT).show();
-                //清楚图层
-                mBaidumap.clear();
-
-                //定义Maker坐标点
-                LatLng point = new LatLng(latitude,longitude);
-                //构建MakerOption，用以在地图上添加Marker
-
-            }
-
-            @Override
-            public boolean onMapPoiClick(MapPoi mapPoi) {
-                return false;
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_map, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.ok:
+                if (latLng == null) {
+                    Toast.makeText(this, "请点击地图选择一个地点！", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                new AlertDialog.Builder(MainActivity.this).setTitle("提示").setMessage("利用基站定位的应用（qq钉钉等）需填写基站信息")
+                        .setPositiveButton("好的", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("package_name", pacakgeName);
+                contentValues.put("latitude", latLng.latitude);
+                contentValues.put("longitude", latLng.longitude);
+                contentValues.put("lac", lac);
+                contentValues.put("cid", cid);
+                mSQLiteDatabase.insertWithOnConflict(DbHelper.APP_TABLE_NAME, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
+                break;
+            case R.id.search:
+                //实现搜索位置功能
+                break;
+            case R.id.lac:
+                View view1 = getLayoutInflater().inflate(R.layout.dialog_lac_cid, null, false);
+                final TextInputEditText etLac = (TextInputEditText) view1.findViewById(R.id.lac);
+                final TextInputEditText etCid = (TextInputEditText) view1.findViewById(R.id.cid);
+                new AlertDialog.Builder(MainActivity.this).setTitle("填写基站信息").setView(view1).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        canCloseDialog(dialog, false);
+                        if (TextUtils.isEmpty(etLac.getText())) {
+                            etLac.setError("lac的值不应该为空");
+                        }
+                        if (TextUtils.isEmpty(etCid.getText())) {
+                            etCid.setError("cid的值不应该为空");
+                        }
+                        if (!TextUtils.isEmpty(etLac.getText()) && !TextUtils.isEmpty(etCid.getText())) {
+                            int lac1 = Integer.parseInt(etLac.getText().toString());
+                            int cid1 = Integer.parseInt(etCid.getText().toString());
+                            if (lac1 <= 0 || lac1 >= 65535) {
+                                etLac.setError("lac的值应该是0~65535");
+                                lac1 = 0;
+                            }
+                            if (cid1 <= 0 || cid1 >= 65535) {
+                                etCid.setError("cid的值应该是0~65535");
+                                cid1 = 0;
+                            }
+                            lac = lac1;
+                            cid = cid1;
+                        }
+                        ContentValues contentValues = new ContentValues();
+                        contentValues.put("package_name", pacakgeName);
+                        contentValues.put("lac", lac);
+                        contentValues.put("cid", cid);
+                        mSQLiteDatabase.insertWithOnConflict(DbHelper.APP_TABLE_NAME, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
+                    }
+                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void canCloseDialog(DialogInterface dialogInterface, boolean close) {
+        try {
+            Field field = dialogInterface.getClass().getSuperclass().getDeclaredField("mShowing");
+            field.setAccessible(true);
+            field.set(dialogInterface, close);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
